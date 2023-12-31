a0a295a52ce4e27654aff19ce6691135181a748e
harshit kumar <harshit.kumar@esamudaay.com>
2023-12-31 20:26:37 +0530

    removing test

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,1 @@
-import 'package:daily_routine_tracker/app/app.dart';
-import 'package:daily_routine_tracker/modules/modules.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  group('App', () {
-    testWidgets('renders CounterPage', (tester) async {
-      await tester.pumpWidget(const App());
-      expect(find.byType(ProfileScreen), findsOneWidget);
-    });
-  });
-}
+void main() {}
